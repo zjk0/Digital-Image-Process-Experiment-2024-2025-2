fbd0ac682320bd5cc51be4ec794e8c12028817a9
zjk0 <2054295479@qq.com>
2025-02-25 11:26:28 +0800

    hello world in tkinter

--- show-image/show_image.py
+++ show-image/show_image.py
@@ -1,1 +1,9 @@
-import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+
+root = Tk()
+frm = ttk.Frame(root, padding = 50)
+frm.grid()
+ttk.Label(frm, text = "Hello World").grid(column = 0, row = 0)
+ttk.Button(frm, text = "Quit", command = root.destroy).grid(column = 1, row = 1)
+root.mainloop()
